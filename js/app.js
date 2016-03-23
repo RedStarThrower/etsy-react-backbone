3785be913c5cc4545caee44a86bcb9c5e00d0296
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-03-23 12:50:47 -0500

    list grid added

--- js/app.js
+++ js/app.js
@@ -35,17 +35,17 @@ import fetch from "isomorphic-fetch"
 
 import DOM from 'react-dom'
 import React, {Component} from 'react'
+import Backbone from 'backbone'
 
 function app() {
     // start app
     // new Router()
     var AppView = React.createClass ({
 
-  //   	componentWillMount: function(){
-		// 	var self = this
-
-		// 	this.props.articles.on('sync',function() {self.forceUpdate()})
-		// },
+    	componentWillMount: function(){
+        	var self = this
+			this.props.etsyData.on('sync',function() {self.forceUpdate()})
+		},
 
     	render: function() {
     		console.log("rendering etsy app")
@@ -59,18 +59,60 @@ function app() {
     })
 
     var ListingGrid = React.createClass({
+
+        _getDataJsx: function(resultsObj) {
+            //console.log(resultsObj)
+            var jsxArray = []
+            for (var prop in resultsObj) {
+                var component = <Listing key={resultsObj[prop].listing_id} listing={resultsObj[prop]}/>
+                jsxArray.push(component)
+            }
+        
+            //console.log(jsxArray)            
+            return jsxArray                 
+        },
+
     	render: function() {
-    		console.log(this)
-    	}
+            //console.log(this)
+            return (
+                <div className="listingGrid">
+                    {this._getDataJsx(this.props.listingData.etsyData.attributes)}
+                </div>
+            )
+      	}
     })
 
-    // var Listing = React.createClass({
-
-    // })
+    var Listing = React.createClass({
+        render: function() {
+        //console.log(this)
+        var listingObj = this.props.listing
+        console.log(listingObj)
+        var imgSrc = "../dist/images/placeholder.png"
+        if(listingObj.Images.length > 0) {
+            imgSrc = listingObj.Images[0].url_170x135
+        }
+            return(
+                <div className="listing">
+                    <div className="home-image">
+                        <img listingId={listingObj.listing_id}src={imgSrc} />
+                    </div>
+                    <div className="title-data"><p className="title">{listingObj.title}</p></div>
+                    <div className="seller-data"><p className="seller">{listingObj.Shop.shop_name}</p>
+                    <p className="price">${listingObj.price}</p>
+                    </div>
+                </div>
+            )
+            
+        }
+    })
 
     var ListModel = Backbone.Model.extend({
-    	_apiKey: apiKey,
-    	url: "https://openapi.etsy.com/v2/listings/active.js?"
+    	_apiKey: "aavnvygu0h5r52qes74x9zvo",
+    	url: "https://openapi.etsy.com/v2/listings/active.js?",
+
+        parse: function(rawJSON) {
+            return rawJSON.results
+        }
     })
 
     var EtsyRouter = Backbone.Router.extend({
@@ -92,7 +134,9 @@ function app() {
         	}
 
      	})
-        promise.then(function(jsonData) {console.log(jsonData)})
+        //promise.then(function(jsonData) {
+            //console.log(jsonData)
+       // })
      	
      	DOM.render(<AppView etsyData={listModel}/>, document.querySelector('.container'))
      },
